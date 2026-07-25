c0a3bfc74fae82e38330d007394bca8ef9622e1f
Maximilian Stahlberg <maximilian.stahlberg@uni-bremen.de>
2026-07-25 22:33:19 +0200

    Add BitSet::iter_combinations.

--- src/lib.rs
+++ src/lib.rs
@@ -415,6 +415,7 @@ impl<W: Word> BitSet<W> {
     /// Generate all subsets, with the maximum number growing slowly.
     ///
     /// See [`Self::subsets_by_size`] for a different iteration order.
+    /// To generate all subsets of `0..=Self::BITS`, use the faster [`Self::iter_all`].
     ///
     /// # Example
     /// ```
@@ -616,7 +617,10 @@ impl<W: Word> BitSet<W> {
         })
     }
 
-    /// Generate all subsets of `0..n`, with the cardinality growing slowly.
+    /// Generate all 2^n subsets of `0..n`, with the cardinality growing slowly.
+    ///
+    /// If `n == Self::BITS` and you do not care about the iteration order, use the faster
+    /// [`Self::iter_all`] for full enumeration.
     ///
     /// # Example
     /// ```
@@ -634,9 +638,33 @@ impl<W: Word> BitSet<W> {
     ///     ])
     /// );
     /// ```
+    // FIXME: Appears to not panic but hang when n > Self::BITS.
     #[inline]
     pub fn iter_all_below(n: usize) -> impl Iterator<Item = Self> {
-        (0..=n).flat_map(move |k| Self::bit_combinations(n, k).map(Self))
+        (0..=n).flat_map(move |k| Self::iter_combinations(n, k))
+    }
+
+    /// Generate all (n choose k) subsets of `0..n` with cardinality k.
+    ///
+    /// The maximum number is growing slowly.
+    ///
+    /// # Example
+    /// ```
+    /// # use pitset::prelude::*;
+    /// assert!(
+    ///     Set::iter_combinations(4, 2).eq([
+    ///         set![0, 1],
+    ///         set![0, 2],
+    ///         set![1, 2],
+    ///         set![0, 3],
+    ///         set![1, 3],
+    ///         set![2, 3],
+    ///     ])
+    /// );
+    /// ```
+    #[inline]
+    pub fn iter_combinations(n: usize, k: usize) -> impl Iterator<Item = Self> {
+        Self::bit_combinations(n, k).map(Self)
     }
 
     // ------------------
