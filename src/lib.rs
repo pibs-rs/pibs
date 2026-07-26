52c16d46ec2b905b3ba2d3570e7b7947e668ce03
Maximilian Stahlberg <maximilian.stahlberg@uni-bremen.de>
2026-07-26 10:41:09 +0200

    Discuss word sizes in the documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -7,27 +7,35 @@
 //! 2. a rich interface for **combinatorics** involving sets of small non-negative integers.
 //!
 //! It is best suited when the bitset should abstract a mathematical set, set operations are
-//! performance critical, and the numbers stored naturally lie in the range `0..=127`.
-//! See [Alternatives](#alternatives) if your use case is different.
+//! performance critical, and the numbers stored naturally lie in the representable range `0..=127`.
+//! See [Alternatives](#alternatives) if your use case differs.
 //!
 //! # Usage
 //!
-//! **TODO**
+//!
+//! `# TODO`
 //!
 //! # Examples
 //!
-//! **TODO**
+//! `# TODO`
 //!
-//! # Alternatives
+//! # Discussion
+//! ## Impact of word size on performance
 //!
-//! The [`BitSet`] offered by this crate uses a single primitive integer type for storage, and
-//! therefore can only store numbers up to 127.
-//! - If your numbers can be larger than 127 but you know an upper bound, consider using
-//!   [fixedbitset](https://docs.rs/fixedbitset) instead.
-//! - If you don't know your largest number ahead of time, [bit-set](https://docs.rs/bit_set) may
-//!   be what you are looking for.
-//! - If you want minimal overhead but only need bit manipulation as opposed to mathematical set
-//!   abstraction, consider [bittle](https://docs.rs/bittle).
+//! Benchmarking suggests that on a 64 bit system, [`BitSet`] operations are about equally fast for
+//! the primitives [`u32`] and [`u64`], while using [`u8`], [`u16`], or [`u128`] for storage makes
+//! them slower by a factor of about two. It is thus recommended to use [`Set128`] only when needed
+//! for capacity, and [`BitSet<u8>`] to [`<u32>`](BitSet<u32>) only when memory use is a concern or
+//! the platform has registers of the corresponding size. The default [`Set`] uses a [`usize`], but
+//! pinning to [`u32`] or [`u64`] can make sense to ensure a consistent capacity across platforms.
+//!
+//! ## Alternatives
+//!
+//! The obvious limitation of this crate is that [`BitSet`] can only store numbers up to 127. If
+//! your numbers can be larger than this but you know an upper bound, consider using
+//! [fixedbitset](https://docs.rs/fixedbitset) or [bittle](https://docs.rs/bittle) instead. If you
+//! don't know your largest number ahead of time, then [bit-set](https://docs.rs/bit_set) may be
+//! what you are looking for.
 
 #![feature(trait_alias)]
 #![no_std]
@@ -35,7 +43,7 @@
 #[cfg(test)]
 mod tests;
 
-/// Re-exports [`BitSet`], [`Set`], [`Set128`], and their associated creation macros.
+/// Re-exports [`BitSet`], [`Set`], [`Set128`], and their creation macros.
 pub mod prelude {
     pub use crate::BitSet;
     pub use crate::Set;
@@ -63,22 +71,22 @@ use core::{
 };
 use num_traits::{CheckedShr, PrimInt, Unsigned, WrappingNeg};
 
-/// Alias for [`BitSet<usize>`], the bitset offering the best performance.
+/// Alias for [`BitSet<usize>`]; the set offering the best performance.
 ///
 /// On 64 bit systems, this set can store integers between 0 and 63 (inclusive).
 /// For numbers up to 127, use [`Set128`] at a potential performance cost.
 pub type Set = BitSet<usize>;
 
-/// Alias for [`BitSet<u128>`], the bitset with the highest capacity.
+/// Alias for [`BitSet<u128>`]; the set with the highest capacity.
 ///
 /// This set can store integers between 0 and 127 (inclusive).
 /// For numbers smaller than the number of bits in a usize, use [`Set`] for best performance.
 pub type Set128 = BitSet<u128>;
 
-/// Alias for [`usize`], the default input and output type for numbers stored in a [`BitSet`].
+/// Alias for [`usize`], the default input and output type for the numbers stored in a [`BitSet`].
 pub type Element = usize;
 
-/// Describes a primitive integer type that [`BitSet`] can use for storage.
+/// A primitive integer that [`BitSet`] can use for storage.
 pub trait Word = PrimInt
     + Unsigned
     + Debug
