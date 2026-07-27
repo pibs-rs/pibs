4a5fde665ca4f13e2de80c3092845c3e0f0d2d2b
Maximilian Stahlberg <maximilian.stahlberg@uni-bremen.de>
2026-07-27 11:54:19 +0200

    Extend and improve the benchmark.

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -1,6 +1,74 @@
-use core::{any::type_name, hint::black_box};
+use core::{any::type_name, hint::black_box, ops::BitOr};
 use criterion::{BenchmarkId, Criterion, Throughput, criterion_group, criterion_main};
 use pitset::*;
+
+/// A word with eight evenly distributed bits set.
+///
+/// The leftmost bit is set. No [`black_box`] is applied.
+///
+/// type    | output
+/// ------- | ------
+/// [`u8`]  | `11111111`
+/// [`u16`] | `1010101010101010`
+/// [`u32`] | `10001000100010001000100010001000`
+/// [`u64`] | `1000000010000000100000001000000010000000100000001000000010000000`
+#[inline(always)]
+fn _evenly_spaced_bitmask<W: Word>() -> W {
+    (0..8)
+        .map(|e| e * size_of::<W>() + size_of::<W>() - 1)
+        .map(|e| W::one() << e)
+        .reduce(BitOr::bitor)
+        .unwrap()
+}
+
+/// A black-boxed set containing eight evenly distributed elements.
+///
+/// Elements correspond to the bits in [`_evenly_spaced_bitmask`], so that the maximum representable
+/// element is present.
+#[inline(always)]
+fn _evenly_spaced_set<W: Word>() -> BitSet<W> {
+    black_box(BitSet::<W>::from_word(_evenly_spaced_bitmask::<W>()))
+}
+
+/// Yield eight black-boxed elements with evenly distributed bit positions in the target primitive.
+///
+/// The bits are the same as are set in [`_evenly_spaced_bitmask`].
+/// ```
+#[inline(always)]
+fn _evenly_spaced_elements<W: Word>() -> impl Iterator<Item = Element> {
+    (0..8)
+        .map(|e| e * size_of::<W>() + size_of::<W>() - 1)
+        .map(black_box)
+}
+
+fn _bench_mutation<W: Word>(
+    group: &mut criterion::BenchmarkGroup<'_, criterion::measurement::WallTime>,
+) {
+    group.throughput(Throughput::Bits(3 * 8));
+    group.bench_function(BenchmarkId::from_parameter(type_name::<W>()), |b| {
+        b.iter(|| {
+            let mut set = BitSet::<W>::new();
+            for e in _evenly_spaced_elements::<W>() {
+                set.insert(e);
+                black_box(&mut set);
+                set.remove(e);
+                black_box(&mut set);
+                set.toggle(e);
+                black_box(&mut set);
+            }
+        });
+    });
+}
+
+fn bench_mutation(criterion: &mut Criterion) {
+    let mut group = criterion.benchmark_group(format!("mutation"));
+    _bench_mutation::<u8>(&mut group);
+    _bench_mutation::<u16>(&mut group);
+    _bench_mutation::<u32>(&mut group);
+    _bench_mutation::<u64>(&mut group);
+    _bench_mutation::<u128>(&mut group);
+    group.finish();
+}
 
 fn _bench_enumerate<W: Word>(
     group: &mut criterion::BenchmarkGroup<'_, criterion::measurement::WallTime>,
@@ -95,19 +163,13 @@ fn bench_iter_all_below(criterion: &mut Criterion) {
 fn _bench_subsets<W: Word>(
     group: &mut criterion::BenchmarkGroup<'_, criterion::measurement::WallTime>,
 ) {
-    /// Generate all sets containing the largest representable element and some subset of 0..7.
-    fn sets<W: Word>() -> impl Iterator<Item = BitSet<W>> {
-        BitSet::<W>::iter_all_below(7).map(|set| black_box(set + BitSet::<W>::MAX))
-    }
-
+    group.throughput(Throughput::Elements(2.pow(8)));
     group.bench_function(
         BenchmarkId::from_parameter(format!("subsets/{}", type_name::<W>())),
         |b| {
             b.iter(|| {
-                for set in sets::<W>() {
-                    for subset in set.subsets() {
-                        black_box(subset);
-                    }
+                for subset in _evenly_spaced_set::<W>().subsets() {
+                    black_box(subset);
                 }
             });
         },
@@ -116,10 +178,8 @@ fn _bench_subsets<W: Word>(
         BenchmarkId::from_parameter(format!("subsets_by_size/{}", type_name::<W>())),
         |b| {
             b.iter(|| {
-                for set in sets::<W>() {
-                    for subset in set.subsets_by_size() {
-                        black_box(subset);
-                    }
+                for subset in _evenly_spaced_set::<W>().subsets_by_size() {
+                    black_box(subset);
                 }
             });
         },
@@ -138,6 +198,7 @@ fn bench_subsets(criterion: &mut Criterion) {
 
 criterion_group!(
     benches,
+    bench_mutation,
     bench_enumerate,
     bench_iter_all_below,
     bench_subsets
